716496050a642a3d2c3c2254a45b063af9fb03bb
Akinades <76144505+Akinades@users.noreply.github.com>
2023-03-16 13:16:17 +0700

    Add files via upload

---
+++ build/precache-manifest.3ec062b3c8c755c037689e76c6b6afe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fafe0bffba7458818d9",
+    "url": "/static/css/main.2e5c6a97.chunk.css"
+  },
+  {
+    "revision": "8fafe0bffba7458818d9",
+    "url": "/static/js/main.8fafe0bf.chunk.js"
+  },
+  {
+    "revision": "1f53fdf60d5828bb510a",
+    "url": "/static/js/1.1f53fdf6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3faf4f39bcff25b376de6bc123acb5b9",
+    "url": "/index.html"
+  }
+];
